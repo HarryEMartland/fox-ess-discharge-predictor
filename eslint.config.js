4100343a752c3e1008b731dbd85140d9c4adee29
Harry Martland <harryemartland@gmail.com>
2026-05-23 11:38:00 +0000

    updated everything added updater

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+const tsParser = require('@typescript-eslint/parser');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
